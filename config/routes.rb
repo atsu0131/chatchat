8dcc70215220759cd25eaea38b7f2b0ba7d35cb1
a <isai@ihitoshitokushinoMacBook-Air.local>
2018-08-16 18:02:42 +0900

    ログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
+
   get 'tops/index'
 
   resources :chats do
