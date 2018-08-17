4d8d42979d475abe6f51fef1b13b054497a9d89c
a <isai@ihitoshitokushinoMacBook-Air.local>
2018-08-17 18:25:03 +0900

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy,:show ]
   resources :users
 
-  get 'tops/index'
+
+  root 'tops#index'
 
   resources :chats do
     collection do
       post :confirm
     end
   end
-  get '/tops', to: 'tops#index'
+
 end
