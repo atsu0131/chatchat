a2656191689d2a259290ae4fa27216c2729e15bf
a <isai@ihitoshitokushinoMacBook-Air.local>
2018-08-17 19:49:14 +0900

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   root 'tops#index'
   resources :sessions, only: [:new, :create, :destroy,:show ]
   resources :users
-
+  
   resources :chats do
     collection do
       post :confirm
     end
   end
-
+  get '/tops', to: 'tops#index'
 end
