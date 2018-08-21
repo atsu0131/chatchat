ef12f3e8032354e93e29db14f728ff2140a3ea1c
a <isai@ihitoshitokushinoMacBook-Air.local>
2018-08-21 18:12:38 +0900

    メッセージ表示修正

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,8 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
+  def contact_mail(contact)
+    @contact = contact
+
+    # 下記を確認
+    mail to: "atsushi.isai131@gmail.com", subject: "お問い合わせの確認メール"
+  end
 end
