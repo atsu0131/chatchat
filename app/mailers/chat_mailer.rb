b36d4697c8b4022e55fd1f3895e70aeacd1ae1af
a <isai@ihitoshitokushinoMacBook-Air.local>
2018-08-22 17:16:05 +0900

    メール機能修正

--- app/mailers/chat_mailer.rb
+++ app/mailers/chat_mailer.rb
@@ -1,6 +1,6 @@
 class ChatMailer < ApplicationMailer
   def chat_mail(chat)
     @chat = chat
-    mail to:"atsushi.isai131@gmail.com", subject:"お問い合わせメール"
+    mail to: @chat.user.email, subject:"お問い合わせメール"
   end
 end
